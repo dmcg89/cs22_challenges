1e44ee42bd0896467faabb49db228b7e6a9bff62
Drew McGowan <michael.mcgowan@students.makeschool.com>
2019-08-05 16:42:24 -0700

    added memoization

--- Challenge4/challenge4.py
+++ Challenge4/challenge4.py
@@ -1,4 +1,17 @@
-def knapsack(C, items, n):
+import functools
+
+def memoize(func):
+    cache = func.cache = {}
+    @functools.wraps(func)
+    def memoized_func(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key not in cache:
+            cache[key] = func(*args, **kwargs)
+        return cache[key]
+    return memoized_func
+
+@memoize
+def knapsack_recursive(C, items, n):
     ''' A  method to determine the maximum value of the items included in the knapsack 
     without exceeding the capacity  C
 
@@ -18,15 +31,39 @@ def knapsack(C, items, n):
     # If weight of the nth item is is more than the knapsack capacity,
     # then this item is not included in optimal solution
     if items[n-1][1] > C:
-        return knapsack(C, items, n-1)
+        print('here')
+        print(items[n-1][1])
+        return knapsack_recursive(C, items, n-1)
 
     else:
-        return max(items[n-1][2] + knapsack(C - items[n-1][1], items, n-1), knapsack(C, items, n-1))
+        print('here2')
+        return max(items[n-1][2] + knapsack_recursive(C - items[n-1][1], items, n-1), knapsack_recursive(C, items, n-1))
+
+def knapsack_dp(C, items, n):
+    K = [[0 for x in range(C+1)] for x in range(n+1)] 
+    for i in range(n+1): 
+        for w in range(C + 1): 
+            if i ==  0 or w == 0: 
+                K[i][w] = 0
+            # weight < capacity
+            elif items[i - 1][1] <= w: 
+                #               value           # look of row, items weight , not using current item
+                K[i][w] = max(items[i - 1][2] + K[i - 1][w - items[i - 1][1]],  K[i-1][w]) 
+            else:
+                # weight > capacity
+                K[i][w] = K[i-1][w]
+    return K[n][C]
+    # return K[n]
 
 C = 50
 items = (("boot", 10, 60),
         ("tent", 20, 100),
         ("water", 30, 120),
-        ("first aid", 15, 70))
+        ("first aid", 15, 70),
+        ('thing', 14, 30),
+        ('otherthing', 8, 22),
+        ('onemorething', 20, 99),
+        ('morethings', 30, 30))
 
-print(knapsack(C, items, len(items)))
+print(knapsack_recursive(C, items, len(items)))
+# print(knapsack_dp(C, items, len(items)))
